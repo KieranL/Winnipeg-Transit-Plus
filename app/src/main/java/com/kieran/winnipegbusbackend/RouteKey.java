815e86f1b1d1e3531c57265460153bd352f845a2
Kieran <kieranloewen@live.com>
2016-05-15 21:36:27 -0500

    Fix for crashing when route variants are not in a route key (I thought they always had one)

--- app/src/main/java/com/kieran/winnipegbusbackend/RouteKey.java
+++ app/src/main/java/com/kieran/winnipegbusbackend/RouteKey.java
@@ -1,24 +1,30 @@
 package com.kieran.winnipegbusbackend;
 
-import com.kieran.winnipegbusbackend.enums.StopTimesNodeTags;
-
 import org.w3c.dom.Node;
 
 import java.io.Serializable;
 
 public class RouteKey implements Serializable {
+    private final static String ROUTE_KEY_TAG = "key";
     private int routeNumber;
     private int routeDirection;
-    private char variant;
+    private char variant = 0;
 
     public RouteKey(Node node) {
-        String[] elements = BusUtilities.getValue(StopTimesNodeTags.KEY.tag, node).split("-");
+        String[] elements = BusUtilities.getValue(ROUTE_KEY_TAG, node).split("-");
         routeNumber = Integer.parseInt(elements[0]);
         routeDirection = Integer.parseInt(elements[1]);
-        variant = elements[2].charAt(0);
+
+        if(elements.length > 2)
+            variant = elements[2].charAt(0);
     }
 
     public String getKeyString() {
-        return routeNumber + "-" + routeDirection + "-" + variant;
+        String key = routeNumber + "-" + routeDirection;
+
+        if(variant != 0)
+            key += "-" + variant;
+
+        return key;
     }
 }
