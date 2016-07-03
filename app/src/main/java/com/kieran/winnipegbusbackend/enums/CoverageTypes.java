23683099b0ee3c7c45261b4b8af91fb5873b351f
Kieran <kieranloewen@live.com>
2016-07-03 16:00:34 -0500

    Added null check to fix crashing issues when no coverage type is provided

--- app/src/main/java/com/kieran/winnipegbusbackend/enums/CoverageTypes.java
+++ app/src/main/java/com/kieran/winnipegbusbackend/enums/CoverageTypes.java
@@ -15,13 +15,17 @@ public enum CoverageTypes implements Serializable {
     }
 
     public static CoverageTypes getEnum(String coverageType) {
-        if(coverageType.equals(CoverageTypes.EXPRESS.typeName))
-            return CoverageTypes.EXPRESS;
-        else if(coverageType.equals(CoverageTypes.SUPER_EXPRESS.typeName))
-            return CoverageTypes.SUPER_EXPRESS;
-        else if(coverageType.equals(CoverageTypes.RAPID_TRANSIT.typeName))
-            return CoverageTypes.RAPID_TRANSIT;
-        else
+        if(coverageType != null) {
+            if (coverageType.equals(CoverageTypes.EXPRESS.typeName))
+                return CoverageTypes.EXPRESS;
+            else if (coverageType.equals(CoverageTypes.SUPER_EXPRESS.typeName))
+                return CoverageTypes.SUPER_EXPRESS;
+            else if (coverageType.equals(CoverageTypes.RAPID_TRANSIT.typeName))
+                return CoverageTypes.RAPID_TRANSIT;
+            else
+                return CoverageTypes.REGULAR;
+        }else {
             return CoverageTypes.REGULAR;
+        }
     }
 }
