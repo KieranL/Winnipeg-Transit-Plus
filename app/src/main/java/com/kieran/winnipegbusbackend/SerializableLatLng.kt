ed860f0fb6570898ea82d7bae528d239efb33789
KieranL <kieranloewen@live.com>
2018-09-04 11:42:27 -0500

    Update version and fix null check

--- app/src/main/java/com/kieran/winnipegbusbackend/SerializableLatLng.kt
+++ app/src/main/java/com/kieran/winnipegbusbackend/SerializableLatLng.kt
@@ -4,9 +4,9 @@ import com.google.android.gms.maps.model.LatLng
 
 import java.io.Serializable
 
-class SerializableLatLng(latLng: LatLng) : Serializable {
-    val latitude: Double = latLng.latitude
-    val longitude: Double = latLng.longitude
+class SerializableLatLng(latLng: LatLng?) : Serializable {
+    val latitude: Double = latLng?.latitude ?: 0.0
+    val longitude: Double = latLng?.longitude ?: 0.0
 
     val latLng: LatLng
         get() = LatLng(latitude, longitude)
