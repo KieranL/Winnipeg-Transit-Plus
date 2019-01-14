76ce38335133cd8230799aa1c6e0162b23028d9d
Kieran Loewen <kieranloewen@live.com>
2019-01-13 23:17:11 -0600

    Bug fix for upcoming stops out of order (#37)

--- app/src/main/java/com/kieran/winnipegbusbackend/ScheduledStopKey.kt
+++ app/src/main/java/com/kieran/winnipegbusbackend/ScheduledStopKey.kt
@@ -9,7 +9,7 @@ class ScheduledStopKey(key: String) : Serializable {
     init {
         val keys = key.split("-".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
         //substring is needed for unexpected characters in some keys, i.e. some keys for Route 75: 7191450NNYNYNN-2
-        busKey = Integer.parseInt(keys[0].substring(0, 7))
+        busKey = Integer.parseInt(keys[0].substring(0, keys[0].lastIndexOfAny(charArrayOf('0', '1', '2', '3', '4','5','6','7', '8','9')) + 1))
         stopNumber = Integer.parseInt(keys[1])
     }
 
